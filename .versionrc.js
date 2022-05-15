a05eca4f24be3465280e060c3f3bbf989e7391d8
xiuxiuyifan <1575243732@qq.com>
2022-05-15 17:06:16 +0800

    chore: 更新type

--- .versionrc.js
+++ .versionrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   "types": [
-    { "type": "feat", "section": "Features" },
-    { "type": "fix", "section": "Bug Fixes" },
-    { "type": "chore", "hidden": true },
-    { "type": "docs", "hidden": true },
-    { "type": "style", "hidden": true },
-    { "type": "refactor", "hidden": true },
-    { "type": "perf", "hidden": true },
-    { "type": "test", "hidden": true }
+    { "type": "feat", "section": "新功能", hidden: false },
+    { "type": "fix", "section": "修复", hidden: false },
+    { "type": "chore", "section": "配置", hidden: false },
+    { "type": "docs", "section": "文档", hidden: true },
+    { "type": "style", "section": "样式", hidden: true },
+    { "type": "refactor", "section": "重构", hidden: true },
+    { "type": "perf", "section": "性能", hidden: true },
+    { "type": "test", "section": "测试", hidden: true },
+    { "type": "revert", "section": "回退", hidden: true },
+    { "type": "build", "section": "构建", hidden: true },
+    { "type": "ci", "section": "部署", hidden: true },
   ]
 }
